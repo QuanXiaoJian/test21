3ae2d23ff03bfd84be94a02f6f180d6a66036a5e
qxj <quanxiaojian1@outlook.com>
2021-09-29 11:10:06 +0800

    属性age

--- stage04test/src/main/java/com/powernode/pojo/Type.java
+++ stage04test/src/main/java/com/powernode/pojo/Type.java
@@ -1,10 +1,11 @@
 public class Type
 {
+	private int age;
 	private String name;
 	public static void main(String[] args){
 	
 		system.out.print("name:");
-		
+		System.out.println("age"+age);
 	}
 
 	public String say(){
